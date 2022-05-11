bbf23457806c1f745081d947fef608ebabbf59e9
Guilherme Daniel <1181743@isep.ipp.pt>
2022-05-11 12:50:51 +0100

    TP6

--- TP6/TP6.R
+++ TP6/TP6.R
@@ -1,3 +1,6 @@
+library(datarium)
+library(rpart)
+library(rpart.plot)
 # Arvore decisao
 # Regressao- atributos objetivos continuos
 # classificacao: atributos objetivos discretos: 1- binaria (2 classes) 2- multi class (> 2)
@@ -49,3 +52,66 @@ BreastCancer$Class <- factor(BreastCancer$Class,
 AbsFreq <- table(BreastCancer$Class)
 PropFreq <- prop.table(table(BreastCancer$Class))*100
 cbind(AbsFreq,PropFreq)
+
+# alinea C
+
+# Metodo holdout
+# Treino: 70%
+# Teste: 30%
+
+# Antes de usar funcao aleatoria, é necessário definir uma seed
+# Sem seed, era sempre gerado um index diferente
+set.seed(1)
+index <- sample(1:nrow(BreastCancer),0.7*nrow(BreastCancer))
+
+data.train <- BreastCancer[index,]
+data.tst <- BreastCancer[-index,]
+
+# True positive é a classe de interesse
+# True negative é a classe que não tem interesse
+# previsões erradas dão falso negativo na classe de não interesse
+# previsões erradas dão falso positivo na classe de interesse
+
+# Taxa de acerto (accuracy) = (True positive + True negative)/(True positive + false negative + false positive + true negative)
+# Taxa de erro = 1 - Taxa de acerto 
+
+# Precision = True Positive / True positive + false positive -> dá a percetagem de previsoes corretas
+# Recall = True positive / True positive + false negative -> quantidade de celulas malignas acertadas
+
+# Precision e Recall são competitivas
+
+#Recall tem que ser maximizado caso seja maligno e for detetado benigno (false negative)
+# Matriz de confusão
+
+#         | ^maligno | ^benigno
+#_________|__________|____________
+#maligno  |    TP    |    FN
+#_________|__________|____________
+#benigno  |    FP    |    TN
+
+# F1= (2*precision*recall)/precision + recall
+
+
+rpart.model <- rpart(Class~.,data=data.train)
+rpart.plot(rpart.model,digits=3)
+rpart.pred <- predict(rpart.model,data.tst,type='class')
+
+#matriz de confusao
+cfmatrix <- table(data.tst$Class,rpart.pred)
+
+#         | ^maligno | ^benigno
+#_________|__________|____________
+#maligno  |    TP    |    FN
+#_________|__________|____________
+#benigno  |    FP    |    TN
+# Taxa de acerto (accuracy) = (True positive + True negative)/(True positive + false negative + false positive + true negative)
+accuracy <- function(cfM) {
+  sum(diag(cfM))/sum(cfM)
+}
+
+accuracy
+
+rmse <- function(test, predict) {
+  sqrt(mean((test - predict) ^ 2))
+}
+
